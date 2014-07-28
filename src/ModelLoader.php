ee6eb6d2fb5a109b7c970e238fdee41fdf81c0f1
Yi Wei <yiwei.in.cyber@gmail.com>
2014-07-28 15:26:49 +0800

    Disable class_exists autoloading

--- src/ModelLoader.php
+++ src/ModelLoader.php
@@ -3,7 +3,7 @@
 class ModelLoader {
 	public static function load($projectName, $modelName) {
 		$model = NULL;
-		if (class_exists($projectName . $modelName)) {
+		if (class_exists($projectName . $modelName, false)) {
 			return $projectName . $modelName;
 		} else {
 			return $modelName;
